d3b43fa1d8cf8f9cb8f1ddd03e83ab6caaf11529
Ceren Şimşek <emresimsek321@gmail.com>
2023-04-06 18:35:40 +0300

    auxilary functions

---
+++ src/utils.c
@@ -0,0 +1,19 @@
+#include "../include/philo.h"
+
+int	my_atoi(char *str)
+{
+	int			i;
+	long int	n;
+
+	i = 0;
+	n = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		n = n * 10 + str[i] - '0';
+		i++;
+	}
+	if (n < 0 || ((str[i] < '0' || str[i] > '9') && str[i] != '\0')
+		|| n > INT_MAX)
+		return (0);
+	return (n);
+}
