14886f7e2f1447c4bcdeba136f33224c5165b2d0
Ceren Şimşek <emresimsek321@gmail.com>
2023-04-06 21:35:31 +0300

    I edited a little more

--- src/main.c
+++ src/main.c
@@ -1,28 +1,5 @@
 #include "../include/philo.h"
 
-int	create_philos(t_vargs *vargs)
-{
-	int	i;
-
-	i = 0;
-	vargs->philos = malloc(sizeof(t_philo) * vargs->inputs.num_of_philo + 1);
-	if (vargs->philos == NULL)
-		return (FALSE);
-	while (i < vargs->inputs.num_of_philo)
-	{
-		vargs->philos[i].id = i + 1;
-		vargs->philos[i].fork.left = 0;
-		vargs->philos[i].fork.right = 0;
-		vargs->philos[i].times_ate = 0;
-		vargs->philos[i].time_to_die = 0;
-		vargs->philos[i].state = STATE_THINKING;
-		vargs->philos[i].philo = malloc(sizeof(pthread_t));
-		i++;
-	}
-	vargs->game_over = FALSE;
-	return (TRUE);
-}
-
 long long	get_time_in_ms(void)
 {
 	struct timeval	time;
@@ -37,8 +14,7 @@ int	main(int ac, char **av)
 
 	if (check_args(&vargs.inputs, av, ac) == FALSE)
 		return (-1);
-	if (create_philos(&vargs) == FALSE)
+	if (create_philos(&vargs, 0) == FALSE)
 		return (-1);
-	
 	return (0);
 }
