f1e03bd9623bc58b294ef1d2a111265dfd3a2211
Ceren Şimşek <emresimsek321@gmail.com>
2023-04-07 17:41:07 +0300

    bla  bla

--- src/main.c
+++ src/main.c
@@ -8,13 +8,17 @@ long long	get_time_in_ms(void)
 	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
+
 int	main(int ac, char **av)
 {
-	t_vargs	vargs;
+	t_vars	vars;
 
-	if (check_args(&vargs.inputs, av, ac) == FALSE)
+	if (check_args(&vars.inputs, av, ac) == FALSE)
 		return (-1);
-	if (create_philos(&vargs, 0) == FALSE)
+	if (create_philos(&vars, 0) == FALSE)
 		return (-1);
+	if (init_mutex(&vars) == FALSE)
+		return (-1);
+	
 	return (0);
 }
