dc31f0fb76704064d8b0518edcbca51b842686ce
Ceren Şimşek <emresimsek321@gmail.com>
2023-04-07 17:41:35 +0300

    init_mutex fucktion xddd

---
+++ src/init_mutex.c
@@ -0,0 +1,22 @@
+#include "../include/philo.h"
+
+int	init_mutex(t_vars *vars)
+{
+	int	i;
+
+	vars->forks = malloc(sizeof(pthread_mutex_t) * vars->inputs.num_of_philo + 1);
+	if (vars->forks == NULL)
+		return (my_errors("something bad happend when mallocing\n"));
+	i = 0;
+	while (i < vars->inputs.num_of_philo)
+	{
+		if (pthread_mutex_init(&vars->forks[i], NULL) != 0)
+			return (FALSE);
+		i++;
+	}
+	if (pthread_mutex_init(&vars->writing_lock, NULL) != 0)
+		return (FALSE);
+	if (pthread_mutex_init(&vars->writing_lock, NULL) != 0)
+		return (FALSE);
+	return (TRUE);
+}
