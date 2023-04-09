16c90039234b021648be02192d4a7ae3a5a9c9dd
Ceren Şimşek <emresimsek321@gmail.com>
2023-04-09 13:40:50 +0300

    Lock and uncluck setted correctly

--- src/dead_check.c
+++ src/dead_check.c
@@ -12,13 +12,11 @@ void	dead_check(t_vars *vars)
 		{
 			pthread_mutex_lock(&vars->philos[i].mutex);
 			if (get_time_in_ms() - vars->philos[i].last_eat_time >= vars->inputs.time_to_die)
-			{
 				is_true = print_philo_state(&vars->philos[i], STATE_DEAD);
-				if (is_true == FALSE)
-					return ;
-			}
-            usleep(vars->inputs.num_of_philo);
             pthread_mutex_unlock(&vars->philos[i].mutex);
+			if (is_true == FALSE)
+				return ;
+            usleep(vars->inputs.num_of_philo);
 		}
 	}
     return ;
